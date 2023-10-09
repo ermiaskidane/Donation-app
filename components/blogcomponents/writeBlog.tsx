edcf2071f477b7a57f3f737ce45fe607ab51e3f5
ermias kidane <ermiaskidanegebre@gmail.com>
2023-10-09 19:27:37 +0100

    make post request

--- components/blogcomponents/writeBlog.tsx
+++ components/blogcomponents/writeBlog.tsx
@@ -1,24 +1,96 @@
 "use client"
 
+import * as z from "zod"
+import axios from "axios"
 import { Category } from '@prisma/client'
 import Image from 'next/image'
 import React, { useState } from 'react'
 import 'react-quill/dist/quill.bubble.css'
 import ReactQuill from 'react-quill'
+import toast from "react-hot-toast"
+import { useRouter } from "next/navigation"
+
+// const formSchema = z.object({
+//   title: z.string().min(1),
+//   catSlug: z.string().min(1),
+//   open: z.boolean()
+// })
+
+// type WriteBlogValues = z.infer<typeof formSchema>
 
 interface WriteBlogProps {
   categories?: Category[]
+}
+interface State {
+  title: string;
+  catSlug: string;
+  open: boolean;
 }
 
 const WriteBlog = ({
   categories
 }: WriteBlogProps) => {
+  const router = useRouter();
 
-  const [open, setOpen] = useState<Boolean>(false)
   const [file, setFile] = useState<File | null>(null)
-  const [value, setValue] = useState<String>("")
-  const [title, setTitle] = useState<String>("")
-  const [catSlug, setCatSlug] = useState<String>("")
+  const [value, setValue] = useState<string>("")
+  const [state, setState] = useState<State>({
+    title: '',
+    catSlug: '',
+    open: false,
+  });
+  const [loading, setLoading] = useState<boolean>(false);
+
+   const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setState((prevState) => ({
+      ...prevState,
+      title: e.target.value,
+    }));
+  };
+
+  const handleChangeCatSlug = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setState((prevState) => ({
+      ...prevState,
+      catSlug: e.target.value,
+    }));
+  };
+
+  const toggleOpen = () => {
+    setState((prevState) => ({
+      ...prevState,
+      open: !prevState.open,
+    }));
+  };
+
+  // Define the slugify function
+const slugify = (str: string): string =>
+str
+  .toLowerCase()
+  .trim()
+  .replace(/[^\w\s-]/g, '')
+  .replace(/[\s_-]+/g, '-')
+  .replace(/^-+|-+$/g, '');
+
+  const handleSubmit = async () => {
+    // submit blog here
+    try {
+      setLoading(true)
+      const response = await axios.post('/api/blog', {
+        title: state.title,
+        desc: value,
+        // img: media,
+        slug: slugify(state.title),
+        catSlug: state.catSlug || 'cloth', // If not selected, choose the general category
+      });
+      router.refresh();
+      router.push('/blog');
+      toast.success("blog posted successfully")
+    } catch(error: any){
+      toast.error('Something went wrong.');
+    } finally{
+      setLoading(false);
+    }
+  }
 
   return (
     <div className='relative flex flex-col'>
@@ -26,12 +98,13 @@ const WriteBlog = ({
         type="text" 
         placeholder='Title' 
         className='p-12 text-6xl border-none outline-none bg-transparent text-red'
-        onChange={(e) => setTitle(e.target.value)}
+        value={state.title}
+        onChange={handleChangeTitle}
        />
        <select 
         className='mb-12 py-2 px-5 w-max'
-        onChange={(e) => setCatSlug(e.target.value)}
-        value={catSlug}
+        onChange={handleChangeCatSlug}
+        value={state.catSlug}
        >
        {categories?.map((cat, i) =>  (
           <option key={i} value={cat.slug}>{cat?.title}</option>
@@ -44,17 +117,22 @@ const WriteBlog = ({
           bg-transparent border-2 border-solid 
           border-gray-400 flex items-center 
           justify-center cursor-pointer"
-          onClick={() => setOpen(!open)}
+          onClick={toggleOpen}
           >
             <Image src='/images/plus.png' alt='' width={16} height={16} />
           </button>
 
-          {open && (
+          {state.open && (
             <div className="flex gap-5 absolute bg-transparent w-screen z-50 left-12">
               <input 
                 type="file"
                 id="image"
-                onChange={(e) => setFile(e.target.files?.[0] || null)}
+                onChange={(e) =>
+                setState((prevState) => ({
+                  ...prevState,
+                  file: e.target.files?.[0] || null,
+                }))}
+                // onChange={(e) => setFile(e.target.files?.[0] || null)}
                 className='hidden' />
 
                 <button className='w-9 h-9 rounded-full 
@@ -89,7 +167,13 @@ const WriteBlog = ({
             onChange={setValue}
             placeholder="Write your Blog..."/>
        </div>
-       <button className="absolute top-0 right-0 py-2 px-5 border-none bg-green-600 text-white cursor-pointer rounded-2xl">
+       <button className="
+        absolute top-0 right-0 py-2 
+        px-5 border-none bg-green-600 
+        text-white cursor-pointer rounded-2xl"
+        onClick={handleSubmit}
+        disabled={loading}
+        >
         Publish
        </button>
     </div>
