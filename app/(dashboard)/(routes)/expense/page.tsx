590d7049a814b78580e244424196fa580ccee936
ermias kidane <ermiaskidanegebre@gmail.com>
2023-11-08 18:01:35 +0000

    style chart graph

--- app/(dashboard)/(routes)/expense/page.tsx
+++ app/(dashboard)/(routes)/expense/page.tsx
@@ -4,53 +4,8 @@ import { db } from '@/lib/db'
 import { initialUser } from '@/lib/initial-user'
 import { redirectToSignIn } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
-import NotifyUser from '@/components/notifyUser'
 
 
-const invoices = [
-  {
-    // expense: "INV002",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    description: "Credit Card PayPal PayPal PayPal",
-  },
-  {
-    // description: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    description: "PayPal",
-  },
-  {
-    // description: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    description: "Bank Transfer",
-  },
-  {
-    // description: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    description: "Credit Card",
-  },
-  {
-    // description: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    description: "PayPal",
-  },
-  {
-    // description: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    description: "Bank Transfer",
-  },
-  {
-    // description: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    description: "Credit Card",
-  },
-]
 
 const ExpensePage = async() =>{
 
@@ -61,9 +16,7 @@ const ExpensePage = async() =>{
   } 
 
   if(user.role === "GUEST"){
-    // redirect("/")
-    // check out later
-    return <NotifyUser/>
+    redirect("/")
   }
   
   const donated = await db.donation.findMany()
