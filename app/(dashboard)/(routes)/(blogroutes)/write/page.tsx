5fd35695f3e94161a888ae1c42cb9ff955d082bc
ermias kidane <ermiaskidanegebre@gmail.com>
2024-05-02 21:59:02 +0100

    chore: delete unneccesarry line code

--- app/(dashboard)/(routes)/(blogroutes)/write/page.tsx
+++ app/(dashboard)/(routes)/(blogroutes)/write/page.tsx
@@ -1,7 +1,7 @@
 import WriteBlog from '@/components/blogcomponents/writeBlog'
 import { currentProfile } from '@/lib/current-profile'
 import { db } from '@/lib/db'
-import { initialUser } from '@/lib/initial-user'
+// import { initialUser } from '@/lib/initial-user'
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import React, { useState } from 'react'
